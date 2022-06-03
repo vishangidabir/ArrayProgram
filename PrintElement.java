2cbd1582fb89b29d4b54937a2205b0dca333f59e
vishangi <vishangid@gmail.com>
2022-06-03 17:05:26 +0530

    Added : print the elements of an array

---
+++ PrintElement.java
@@ -0,0 +1,14 @@
+package ArrayProgram;
+
+public class PrintElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = new int[] { 1, 2, 3, 4, 5 };
+		System.out.println("Print Array Element :");
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+	}
+
+}
