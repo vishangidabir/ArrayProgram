f896a71f297b513ee3cfb3977164090da462b620
vishangi <vishangid@gmail.com>
2022-06-03 17:07:51 +0530

    Added : print the elements of an array in reverse order

---
+++ ReverseElement.java
@@ -0,0 +1,20 @@
+package ArrayProgram;
+
+public class ReverseElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = new int[] { 1, 2, 3, 4, 5 };
+
+		System.out.print("Element of Array :");
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.print("\nReserve Element of Array :");
+		for (int i = array.length - 1; i >= 0; i--) {
+			System.out.print(array[i] + " ");
+		}
+	}
+
+}
